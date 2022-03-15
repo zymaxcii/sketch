1e60322c494b2bbb304fdccd0265f0c97a122b70
zymaxcii <zyma.xcii@gmail.com>
2022-03-15 16:35:46 +0800

    added rfid and ds18b20

--- Santos/code/13 - MRFC522 RFID/Code/MFRC522_RFID_Reader_with_Arduino/MFRC522_RFID_Reader_with_Arduino.ino
+++ Santos/code/13 - MRFC522 RFID/Code/MFRC522_RFID_Reader_with_Arduino/MFRC522_RFID_Reader_with_Arduino.ino
@@ -1,3 +1,5 @@
+// RFID
+
 /*
  * 
  * All the resources for this project: http://randomnerdtutorials.com/
@@ -9,20 +11,22 @@
  
 #include <SPI.h>
 #include <MFRC522.h>
- 
+
 #define SS_PIN 10
 #define RST_PIN 9
-MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance.
- 
+MFRC522 mfrc522(SS_PIN, RST_PIN);   // Create MFRC522 instance
+
+
 void setup() 
 {
-  Serial.begin(9600);   // Initiate a serial communication
-  SPI.begin();      // Initiate  SPI bus
+  Serial.begin(9600);
+  SPI.begin();          // Initiate SPI bus
   mfrc522.PCD_Init();   // Initiate MFRC522
   Serial.println("Approximate your card to the reader...");
   Serial.println();
-
 }
+
+
 void loop() 
 {
   // Look for new cards
@@ -30,15 +34,18 @@ void loop()
   {
     return;
   }
+ 
   // Select one of the cards
   if ( ! mfrc522.PICC_ReadCardSerial()) 
   {
     return;
   }
-  //Show UID on serial monitor
+ 
+  // Show UID on serial monitor
   Serial.print("UID tag :");
-  String content= "";
+  String content = "";
   byte letter;
+  
   for (byte i = 0; i < mfrc522.uid.size; i++) 
   {
      Serial.print(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " ");
@@ -46,18 +53,22 @@ void loop()
      content.concat(String(mfrc522.uid.uidByte[i] < 0x10 ? " 0" : " "));
      content.concat(String(mfrc522.uid.uidByte[i], HEX));
   }
+  
   Serial.println();
   Serial.print("Message : ");
   content.toUpperCase();
-  if (content.substring(1) == "BD 31 15 2B") //change here the UID of the card/cards that you want to give access
+  
+  // change here the UID of the card/cards that you want to give access
+  if (content.substring(1) == "BD 31 15 2B")
   {
-    Serial.println("Authorized access");
-    Serial.println();
-    delay(3000);
+     Serial.println("Authorized access");
+     Serial.println();
+     delay(3000);
   }
  
- else   {
-    Serial.println(" Access denied");
-    delay(3000);
+  else
+  {
+     Serial.println(" Access denied");
+     delay(3000);
   }
 }
--- Santos/code/5 - DS 18B20 Temperature/Code/Example_Temperature/Example_Temperature.ino
+++ Santos/code/5 - DS 18B20 Temperature/Code/Example_Temperature/Example_Temperature.ino
@@ -1,3 +1,5 @@
+// Example_Temperature.ino
+
 #include <DallasTemperature.h>
 
 /*********
@@ -9,7 +11,7 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-// Data wire is conntec to the Arduino digital pin 2
+// Data wire is connected to Arduino digital pin 2
 #define ONE_WIRE_BUS 2
 
 // Setup a oneWire instance to communicate with any OneWire devices
@@ -27,12 +29,15 @@ void setup(void)
 }
 
 
-void loop(void){ 
-  // Call sensors.requestTemperatures() to issue a global temperature and Requests to all devices on the bus
+void loop(void)
+{ 
+  // Call sensors.requestTemperatures() to issue a global temperature
+  // and Requests to all devices on the bus
   sensors.requestTemperatures(); 
   
   Serial.print("Celsius temperature: ");
-  // Why "byIndex"? You can have more than one IC on the same bus. 0 refers to the first IC on the wire
+  // Why "byIndex"? You can have more than one IC on the same bus
+  // 0 refers to the first IC on the wire
   Serial.print(sensors.getTempCByIndex(0)); 
   Serial.print(" - Fahrenheit temperature: ");
   Serial.println(sensors.getTempFByIndex(0));
