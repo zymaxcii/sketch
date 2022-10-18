0f31ec7d2cd90bb5245618711c5b34912b88b67f
Ma Tit <mty@blabs.yap.com>
2022-10-18 19:19:15 +0800

    clean up tm1638 folder

--- basic/tm1638/tm1638_count100.ino
+++ basic/tm1638/tm1638_count100.ino
@@ -1,23 +1,28 @@
 // tm1638_count100.ino
+
 // http://arduinolearning.com/learning/basics/arduino-tm1638-module.php
 
 
 #include <TM1638.h>
+
 // define a module on data pin 8, clock pin 9 and strobe pin 10
 TM1638 module(8, 9, 10);
-unsigned long a=1;
- 
+
+unsigned long a = 1;
+
+
 void setup()
 {
 }
+
  
 void loop()
 {
-for (a=1; a<=100; a++)
-{
-module.setDisplayToDecNumber(a,0,false);
-delay(100);
-}
-module.setDisplayToString("Complete");
-delay(1000);
+  for (a=1; a<=100; a++)
+  {
+    module.setDisplayToDecNumber(a, 0, false);
+    delay(100);
+  }
+  module.setDisplayToString("Complete");
+  delay(1000);
 }
