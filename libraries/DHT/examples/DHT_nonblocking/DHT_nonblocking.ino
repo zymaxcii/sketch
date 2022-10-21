9e76396aa496620bfc3b219372daf69a78c8afee
zymaxcii <zyma.xcii@gmail.com>
2022-10-21 15:44:28 +0800

    played with elegoo sensors

--- libraries/DHT/examples/DHT_nonblocking/DHT_nonblocking.ino
+++ libraries/DHT/examples/DHT_nonblocking/DHT_nonblocking.ino
@@ -1,11 +1,34 @@
-//www.elegoo.com
-//2018.10.25
+/*
+ * Example of a non-blocking read of DHTxx sensor values.
+ *
+ * The sensor is connected to the Arduino with a 10 kOhm pull-up
+ * resistor connected to the data pin.
+ *
+ * (C) 2015 Ole Wolf <wolf@blazingangles.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 
 #include <dht_nonblocking.h>
-#define DHT_SENSOR_TYPE DHT_TYPE_11
 
-static const int DHT_SENSOR_PIN = 5;
+/* Uncomment according to your sensortype. */
+#define DHT_SENSOR_TYPE DHT_TYPE_11
+//#define DHT_SENSOR_TYPE DHT_TYPE_21
+//#define DHT_SENSOR_TYPE DHT_TYPE_22
+
+static const int DHT_SENSOR_PIN = 2;
 DHT_nonblocking dht_sensor( DHT_SENSOR_PIN, DHT_SENSOR_TYPE );
 
 
@@ -62,3 +85,4 @@ void loop( )
     Serial.println( "%" );
   }
 }
+
